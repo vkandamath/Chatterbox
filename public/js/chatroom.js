f7a0bb13adfe447819fd5b266c10db170ebcf769
Vyshakh Kandamath <kvyshakh@terpmail.umd.edu>
2017-01-08 16:16:15 -0500

    styling

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,25 +1,32 @@
 var socket = io();
 
+$("#message-form").submit(function(e) {
+	e.preventDefault();
+});
+
 socket.on('connected', function(msg) {
-	$("#messages-list").append("<li>User " + msg.userid + "has connected.</li>");
+	$("#messages ul").append("<li><font color='green'>User " + msg.userid + "has connected.</font></li>");
 });
 
 socket.on('myUserId', function(msg) {
-	$("#message-label").append("User " + msg.userId + ": ");
+	$("#user").append("User " + "<font color='#337AB7'>" + msg.userId + "</font>");
 })
 
 socket.on('disconnected', function(msg) {
-	$("#messages-list").append("<li>User " + msg.userid + "has disconnected.</li>");
+	$("#messages ul").append("<li>User " + msg.userid + "has disconnected.</li>");
 });
 
 socket.on('incoming message', function(msg){
     console.log(msg);
-    $("#messages-list").append("<li>User " + msg.userid + ": " + msg.message + "</li>");
+    $("#messages ul").append("<li>User " + msg.userid + ": " + msg.message + "</li>");
 });
 
 
 function sendMessage() {
 	var message = $("#message").val();
-	$("#message").val('');
-   	socket.emit('outgoing message', {message: message, userid: socket.id});
+	
+	if (message != '') {
+		$("#message").val('');
+   		socket.emit('outgoing message', {message: message, userid: socket.id});
+	}
 }

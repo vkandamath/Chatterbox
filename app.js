ef2e6df71d5d33bdf61cb36a02c20e68917bc388
Vyshakh Kandamath <kvyshakh@terpmail.umd.edu>
2017-11-23 23:41:19 -0500

    fixed bug where source language was undefined

--- app.js
+++ app.js
@@ -249,7 +249,7 @@ io.on('connection', function(socket) {
 							console.log(err)
 						}
 						else {
-							io.in(room_id).emit("changed user properties", {old_username: old_username, new_username: user.username, room_members: chatroom.members})
+							io.in(room_id).emit("changed user properties", {old_username: old_username, new_username: user.username, language: user.language, room_members: chatroom.members})
 						}
 					})
 				})
